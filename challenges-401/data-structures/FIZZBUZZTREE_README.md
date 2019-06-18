606fcc36bba437683f61c4254b6890e2dce00411
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-06-17 20:38:45 -0700

    Added readme

---
+++ challenges-401/data-structures/FIZZBUZZTREE_README.md
@@ -0,0 +1,20 @@
+# FizzBuzzTree
+Conduct “FizzBuzz” on a tree while traversing through it. Change the values of each of the nodes dependent on the current node’s value.
+## Challenge
+Create a fizzBuzzTree method that converts tree values into:
+ 1. "Fizz" if divisible by 3
+ 2. "Buzz" if divisible by 5
+ 3. "FizzBuzz" if divisible by 3 and 5
+ 4. No change if none of the above applies
+ 
+## Approach and Efficiency
+Used the recursion with pre-order traversal to check and convert each node. Each node string data was converted to Integer and then evaluated using mod operation to check divisibility.Time and space complexities: 0(n) with the recursion.
+## API
+1. fizzBuzzTree - accepts Tree with String datatype.
+Usage example: 
+`Tree<String> numbers = new Tree<>(new Node("20"));`
+`FizzBuzzTree.fizzBuzzTree(numbers);`
+       
+## Source Code
+* [FizzBuzzTree](./src/main/java/FizzBuzzTree/FizzBuzzTree.java)
+* [FizzBuzzTreeTest](./src/test/java/FizzBuzzTree/FizzBuzzTreeTest.java)
--- challenges-401/data-structures/src/main/java/FizzBuzzTree/FizzBuzzTree.java
+++ challenges-401/data-structures/src/main/java/FizzBuzzTree/FizzBuzzTree.java
@@ -6,18 +6,29 @@ import tree.Tree;
 
 public class FizzBuzzTree {
 
+    //For numbers divisible by 3
     private static final String FIZZ = "Fizz";
+    //For numbers divisible by 5
     private static final String BUZZ = "Buzz";
+    //For numbers divisible by 3 and 5
     private static final String FIZZBUZZ = "FizzBuzz";
 
 
+    /**
+     * Method to convert values based from divisibility of 3 and 5.
+     * @param input tree values to be converted
+     */
     public static void fizzBuzzTree(Tree<String> input){
         fizzBuzzRecursion(input.getOverallRoot());
     }
 
+    //Helper method for fizzbuzz conversion
     private static void fizzBuzzRecursion(Node<String> root){
+        //Check if node is not null; If root is null do nothing
         if(root != null){
+            // get root value and convert to Integer
             Integer rootValue = Integer.parseInt(root.getData());
+            //Check if divisible by 3 and/or 5, if true set to corresponding FizzBuzz
             if(isDivisibleBy3(rootValue) && isDivisibleBy5(rootValue)){
                 root.setData(FIZZBUZZ);
             } else if(isDivisibleBy3(rootValue)){
@@ -26,17 +37,21 @@ public class FizzBuzzTree {
                 root.setData(BUZZ);
             }
 
+            // Check left subtree
             fizzBuzzRecursion(root.getLeft());
+            //Check right subtree
             fizzBuzzRecursion(root.getRight());
         }
 
 
     }
 
+    // Helper method to check if value divisible by 3
     private static boolean isDivisibleBy3(Integer value){
        return (value % 3 == 0);
     }
 
+    // Helper method to check if value divisible by 5
     private static boolean isDivisibleBy5(Integer value){
         return (value % 5 == 0);
     }
